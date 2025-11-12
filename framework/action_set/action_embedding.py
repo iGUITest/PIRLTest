4a455baf84a7e1c5c9616b82a888d40e5e147b8c
Mar-garet <742862436@qq.com>
2025-11-12 12:45:23 +0800

    add necessary comment

--- framework/action_set/action_embedding.py
+++ framework/action_set/action_embedding.py
@@ -4,14 +4,17 @@ from img_process.widget import widgets_embedding as we
 
 
 def embed_action_type(action_types):
+    """One-hot encode action types."""
     return np.eye(ActionType.count())[action_types]
 
 def embed_window_size(window_sizes, max_width, max_height):
+    """Embed window sizes relative to max dimensions."""
     sizes_vector = np.expand_dims(np.array(window_sizes), axis=1)
     base_vector = np.array([[max_width, max_height, 1]])
     return sizes_vector * base_vector
 
 def embed_actions_no_arg(actions, type_embed_size=200):
+    """Embed actions without arguments."""
     action_types = [a[0] for a in actions]
     atype_embeddings = embed_action_type(action_types)
     # type_embed_size is for widget type.
@@ -19,17 +22,19 @@ def embed_actions_no_arg(actions, type_embed_size=200):
     return np.concatenate([atype_embeddings, remainings], axis=1)
 
 def embed_actions_with_widget(actions, image, type_embed_size=200):
+    """Embed actions with widget information."""
     atype_embeddings = embed_action_type([a[0] for a in actions])
     bboxes = [a[1] for a in actions]
     widget_img_embeddings = we.embed_img([image[y:y+h, x:x+w, :] for x, y, w, h in bboxes])
     widget_bbox_embeddings = we.embed_bbox(bboxes, image.shape[1], image.shape[0])
     widget_type_embeddings = np.eye(type_embed_size)[[a[2] for a in actions]]
     remainings = np.zeros((len(actions), 3))
-    return np.concatenate([atype_embeddings, widget_img_embeddings, 
-                           widget_bbox_embeddings, widget_type_embeddings, 
+    return np.concatenate([atype_embeddings, widget_img_embeddings,
+                           widget_bbox_embeddings, widget_type_embeddings,
                            remainings], axis=1)
 
 def embed_actions_with_window(actions, max_width, max_height, type_embed_size=200):
+    """Embed actions with window information."""
     atype_embeddings = embed_action_type([a[0] for a in actions])
     widget_embeddings = np.zeros((len(actions), 4096 + 4096 + type_embed_size))
     window_embeddings = embed_window_size([a[1] for a in actions], max_width, max_height)
